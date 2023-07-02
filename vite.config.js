fef2f83404a842474e7baa20204b49471ae3525a
yassine w <yassine3107@hotmail.com>
2023-07-02 19:29:14 +0200

    d

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/',
+  base: '/EPCapp/',
   build: {
     assetsDir: '',
     outDir: 'docs',
